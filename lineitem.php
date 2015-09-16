3fd37557e9296bab6e1f82cecb0adb2e9536efd5
jacob <anseo@forseti.dreamhost.com>
2015-09-16 08:26:11 -0700

    Added Price to lineitem structure and sample code

--- lineitem.php
+++ lineitem.php
@@ -10,7 +10,7 @@ class LineItem implements JsonSerializable
 			            'item' => [
 				                    'id' => $this->id
 						          ]
-						//'price' => $this->price
+						
 						];   
 			}
 }
