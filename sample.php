d94850d4cf5ded7f45aca23f14c5713dab50e850
thejuicerypress <info@thejuicerypress.com>
2015-09-16 09:26:53 -0500

    Create sample.php

---
+++ sample.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'create.order.php';
+
+//create order request
+$order = new Order();
+$order->note = 'order note';
+$order->title = 'order title';
+$order->taxRemoved = false;
+$order->orderTypeId = 'AKK5DXMJCBPSW';
+
+$newOrder = createOrder($order);
+
+//create a line items
+$lineItem = new LineItem();
+$lineItem->id = 'XE9JWHPXBAQR8';
+
+//add line item to order
+addItemToOrder($newOrder, $lineItem);
+
+//open order
+$orderStatus = openOrder($newOrder);
+
+print_r($orderStatus);
+
+?>
