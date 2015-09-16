a6ce543697b14d19836412ef17beb9f31706c191
thejuicerypress <info@thejuicerypress.com>
2015-09-16 09:29:43 -0500

    Create order.php

---
+++ order.php
@@ -0,0 +1,22 @@
+<?php
+
+class Order implements JsonSerializable
+{
+    public $note;
+    public $title;
+	public $taxRemoved;
+	public $orderTypeId;
+		    
+	public function jsonserialize()
+		{
+			return [
+				        'note' => $this->note,
+						'orderType' => [
+							                'id' => $this->orderTypeId
+									    ],
+						'title' => $this->title,
+						'taxRemoved' => $this->taxRemoved
+					];   
+		}
+ }
+?>
